567a3669e807528308fd847b829878903a13b211
TestKatCode <79061719499@yandex.ru>
2023-10-17 16:22:02 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://fad56420-e3b9-4fd5-9ff1-a9dbc38ce30a.serverhub.praktikum-services.ru"
+DOC_PATH = "https://fad56420-e3b9-4fd5-9ff1-a9dbc38ce30a.serverhub.praktikum-services.ru"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ create_user_test.py
@@ -0,0 +1,7 @@
+import sender_stand_request
+import data
+
+
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Ааааааааааааааа")
+    
